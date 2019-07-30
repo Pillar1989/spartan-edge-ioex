681fa680032272cd12df1b97b49ecfca778a09a8
SJ_K <137526100@qq.com>
2019-07-30 11:22:10 +0800

    encapsulating some function

--- examples/02RGB_LED/02RGB_LED.ino
+++ examples/02RGB_LED/02RGB_LED.ino
@@ -20,33 +20,18 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  static unsigned long color = 0xFFUL;
-  
-  /* set RGB1 value B->R->G */
-  A.regWrite(SK6805_CTRL, 0x0);
-  A.regWrite(SK6805_DATA, (color >>  3) & 0x1F);// blue
-  A.regWrite(SK6805_CTRL, 0x1);
-  A.regWrite(SK6805_DATA, (color >> 11) & 0x1F);// red
-  A.regWrite(SK6805_CTRL, 0x2);
-  A.regWrite(SK6805_DATA, (color >> 19) & 0x1F);// green
+  // set RGBled1/2 green
+  A.setRGBLedVal(0, 0, 50, 0);
+  A.setRGBLedVal(1, 0, 50, 0);
+  delay(1000);
 
-  /* set RGB2 value R->G->B */
-  A.regWrite(SK6805_CTRL, 0x3);
-  A.regWrite(SK6805_DATA, (color >> 19) & 0x1F);// blue
-  A.regWrite(SK6805_CTRL, 0x4);
-  A.regWrite(SK6805_DATA, (color >>  3) & 0x1F);// red
-  A.regWrite(SK6805_CTRL, 0x5);
-  A.regWrite(SK6805_DATA, (color >> 11) & 0x1F);// green
+  // set RGBled1/2 red
+  A.setRGBLedVal(0, 50, 0, 0);
+  A.setRGBLedVal(1, 50, 0, 0);
+  delay(1000);
 
-  /* set color value */
-  switch (color) {
-  case 0x0000FFUL: color = 0x00FF00UL; break;
-  case 0x00FF00UL: color = 0xFF0000UL; break;
-  case 0xFF0000UL: color = 0x0000FFUL; break;
-  default:
-    color = 0xFFUL; break;
-  }
-  
-  // delay in between reads for stability
-  delay(1500);    
+  // set RGBled1/2 blue
+  A.setRGBLedVal(0, 0, 0, 50);
+  A.setRGBLedVal(1, 0, 0, 50);
+  delay(1000);
 }
--- examples/03ADC_AND_DAC/03ADC_AND_DAC.ino
+++ examples/03ADC_AND_DAC/03ADC_AND_DAC.ino
@@ -17,7 +17,8 @@ spartan_edge_ioex A;
 // the setup routine runs once when you press reset:
 void setup() {
   int v;
-  
+
+  // initialize serial communication at 115200 bits per second: 
   Serial.begin(115200);
 
   /* Enable ADC1173, set /OE to LOW */
@@ -27,48 +28,13 @@ void setup() {
   A.regWrite(GPE_ODATA, v);
 }
 
-/* read ADC_data and return Voltage */
-unsigned long /* Voltage(mv) */readAdcData(void){
-  int adcData;
-  unsigned long voltage;
-  
-  // read ADC value
-  adcData = A.regRead(ADC_DATA); 
-
-  /*
-   * ADC_data Transform to  Voltage(mv)
-   * if yu want to know detail,
-   * you can come [http://www.ti.com/product/ADC1173]
-   */
-  voltage = (unsigned long)adcData * 3300 / 256; 
-  
-  return voltage;
-}
-
-/* write Voltage(mv) to DAC */
-void writeDacData(unsigned int voltVal/* Voltage(mv) */){
-  unsigned long dacData;
-  
-  /*
-   * Voltage(mv) Transform to ADC_data
-   * if yu want to know detail,
-   * you can come [http://www.ti.com/product/DAC7311]
-   */
-  dacData = (unsigned long)voltVal * 4096 / 3300;
-  
-  // DATA1 first
-  A.regWrite(DAC_DATA1, (dacData >> 6) & 0x3F);
-  // DATA0 last
-  A.regWrite(DAC_DATA0, (dacData << 2) & 0xFC);
-}
-
 // the loop routine runs over and over again forever:
 void loop() {
   unsigned long voltVal;
   int adcData;
 
   // read ADC value
-  voltVal = readAdcData();
+  voltVal = A.readAdcData();
   
   /* output Voltage(ms) by Serial */
   Serial.print("ADC : ");
@@ -77,7 +43,7 @@ void loop() {
 
   // output DAC val
   // DAC-OUT = ADC-IN ,input DAC value ,which is what you read before 
-  writeDacData(voltVal);
+  A.writeDacData(voltVal);
   
   // Change other line
   Serial.println();  
